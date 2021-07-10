426c49b97ee5e1be9d3c642fac0b3fcecb73c577
Boggil <boggil3665@daum.net>
2021-07-10 23:21:42 +0900

    Fix: initial index.ts

--- back/index.ts
+++ back/index.ts
@@ -1,1 +1,13 @@
 import * as express from 'express';
+
+const app = express();
+const prod: boolean = process.env.NODE_ENV === 'production';
+
+app.set('port', prod ? process.env.PORT : 3065);
+app.get('/', (req, res, next) => {
+  res.send('react nodebrind running!');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`server is running on ${app.get('port')}`);
+});
